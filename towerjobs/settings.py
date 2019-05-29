60984f9c4987ce8383e072fdb22a183d95302915
revjatto <revjatto@yahoo.co.uk>
2019-05-30 00:07:22 +0100

    another changes

--- towerjobs/settings.py
+++ towerjobs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qvnes)*)0+zie&p6vb@wt7br-ou0ohf4!@j2sa(-y__5qnv^-r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tower-jobs.herokuapp.com' ]
 
 
 # Application definition
